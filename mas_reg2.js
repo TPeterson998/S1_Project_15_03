1ea6d9b8d444981b5de689513fe90e9e454d6f46
Trent Peterson <tpeterson998@west-mec.org>
2019-04-23 15:28:10 -0700

    Day 2
    I finished the code. But it is still not working. I got help from Brendon and Paige. After a while we put the code through a text comparer and the codes are almost identical with just comments and some of the names of variables that are wrong. I am glad I have one more day.

--- mas_reg2.js
+++ mas_reg2.js
@@ -20,3 +20,18 @@
 
 
 */
+window.onload = function () {
+      writeSessionValues();
+}
+
+function writeSessionValues() {
+      //All of these take the info that was gathered above and set it to several spans 
+      document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+      document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+      document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+      document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+      document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+      document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+      document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
+}
--- mas_register.js
+++ mas_register.js
@@ -27,16 +27,75 @@
 
 
 */
-
+//this loads my stuff when the window starts
 window.onload = function () {
       calcCart();
-      document.getElementById("regSubmit").onclick = sessionTest();
-      document.getElementById("fnBox").onblur = calcCart();
-      document.getElementById("lnBox").onblur = calcCart();
-      document.getElementById("groupBox").onblur = calcCart();
-      document.getElementById("mailBox").onblur = calcCart();
-      document.getElementById("phoneBox").onblur = calcCart();
-      document.getElementById("banquetBox").onblur = calcCart();
+      //loads the session test function when the submit button is clicked
+      document.getElementById("regSubmit").onclick = sessionTest;
+      //These load the calcCart when 
+      document.getElementById("fnBox").onblur = calcCart;
+      document.getElementById("lnBox").onblur = calcCart;
+      document.getElementById("groupBox").onblur = calcCart;
+      document.getElementById("mailBox").onblur = calcCart;
+      document.getElementById("phoneBox").onblur = calcCart;
+      document.getElementById("banquetBox").onblur = calcCart;
+      //this loads the calcCart when the session box changes
+      document.getElementById("sessionBox").onchange = calcCart;
+      //this loads the calcCart function when you click the mediaCb
+      document.getElementById("mediaCB").onclick = calcCart;
+}
 
+function sessionTest() {
+      //this sets a custom validity if their is no option selected in the select box
+      var session = document.getElementById("sessionBox");
+      if (session.selectedIndex === -1) {
+            session.setCustomValidity("Select a Session Package");
+      } else {
+            session.setCustomValidity("");
+      }
+}
 
+function calcCart() {
+
+      //These get the value of the things that the user inputs into the form
+      sessionStorage.setItem("confName", document.getElementById("fnBox").value + " " + document.getElementById("lnBox").value);
+      sessionStorage.setItem("confGroup", document.getElementById("groupBox").value);
+      sessionStorage.setItem("confMail", document.getElementById("mailBox").value);
+      sessionStorage.setItem("confPhone", document.getElementById("phoneBox").value);
+      sessionStorage.setItem("confBanquet", document.getElementById("banquetBox").value);
+      //this gets the cost for how many banquets the user selected
+      sessionStorage.setItem("confBanquetCost", document.getElementById("banquetBox").value * 55);
+      //this gets the cost of what ever the selection list is
+      var session = document.getElementById("sessionBox");
+      if (session.selectedIndex !== -1) {
+            sessionStorage.setItem("confSession", session[session.selectedIndex].innerText);
+            sessionStorage.setItem("confSessionCost", session[session.selectedIndex].value);
+      } else {
+            sessionStorage.setItem("confSession", " ");
+            sessionStorage.setItem("confSessionCost", 0);
+      }
+      //This gets the cost of the checked box if it is checked
+      var checkBox = document.getElementById("mediaCB");
+      if (checkBox.checked == true) {
+            sessionStorage.setItem("confPack", "yes");
+            sessionStorage.setItem("confPackCost", 115);
+      } else {
+            sessionStorage.setItem("confPack", "no");
+            sessionStorage.setItem("confPackCost", 0);
+      }
+      //This adds the costs of several things
+      sessionStorage.setItem("confTotal", parseFloat(sessionStorage.getItem("confSessionCost")) + parseFloat(sessionStorage.getItem("confBanquetCost")) + parseFloat(sessionStorage.getItem("confPackCost")));
+      writeSessionValues();
+}
+
+function writeSessionValues() {
+      //All of these take the info that was gathered above and set it to several spans 
+      document.getElementById("regName").textContent = sessionStorage.getItem("confName");
+      document.getElementById("regGroup").textContent = sessionStorage.getItem("confGroup");
+      document.getElementById("regEmail").textContent = sessionStorage.getItem("confMail");
+      document.getElementById("regPhone").textContent = sessionStorage.getItem("confPhone");
+      document.getElementById("regSession").textContent = sessionStorage.getItem("confSession");
+      document.getElementById("regBanquet").textContent = sessionStorage.getItem("confBanquet");
+      document.getElementById("regPack").textContent = sessionStorage.getItem("confPack");
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.getItem("confTotal");
 }
